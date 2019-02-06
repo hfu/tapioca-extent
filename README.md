55ab52821f1172575f8abbc5025f3465c5c5e549
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-02-05 20:30:16 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tapioca-extent
 just a small code to calculate the extent of tapioca
+
+# install and run
+```console
+git clone git@github.com:hfu/tapioca-extent
+cd tapioca-extent
+npm install
+node index.js
+```
+
---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+// tile number to lng/lat
+const ttl = (zxy) => {
+  const n = Math.PI - 2 * Math.PI * zxy[2] / Math.pow(2, zxy[0])
+  return [
+    zxy[1] / Math.pow(2, zxy[0]) * 360 - 180,
+    (180 / Math.PI * Math.atan(
+      0.5 * (Math.exp(n) - Math.exp(-n))))
+  ]
+}
+
+const [west, north] = ttl([6, 22, 24])
+const [east, south] = ttl([6, 43, 40])
+
+const f = {
+  type: 'Feature',
+  geometry: {
+    type: 'LineString',
+    coordinates: [[
+      [west, south],
+      [east, south],
+      [east, north],
+      [west, north],
+      [west, south]
+    ]]
+  },
+  properties: {}
+}
+
+fs.writeFileSync('extent.json', JSON.stringify(f, null, 2))
